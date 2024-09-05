052f4bfee411bb155c0e1f425e8147f0915ae616
AG <anna.gabain@gmail.com>
2024-09-05 16:00:44 +0200

    add basic styling to the 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import '../NotFound.css'
 
 export const NotFound = () => {
-    return (
-      <div className="card">
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <h2  style ={{backgroundColor:'black'}}><Link to="/">Go back to Home</Link></h2>
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="error-code">404</h1>
+        <p className="error-message">Oops! The page you are looking for does not exist.</p>
+        <Link to="/" className="home-link">Go back to Home</Link>
       </div>
-    );
-  };
-  
-  
+    </div>
+  );
+};
