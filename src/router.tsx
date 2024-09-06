af283243371dc4d88901c330e5b7fc01deeedc31
AG <anna.gabain@gmail.com>
2024-09-06 09:14:20 +0200

    refactor the routing configuration

--- src/router.tsx
+++ src/router.tsx
@@ -1,23 +1,37 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from 'react-router-dom';
+
 import App from './App';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { NotFound } from './components/NotFound';
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            { path: '', element: <Home /> },
-            { path: 'about', element: <About /> },
-            { path: 'contact', element: <Contact /> },
-            { path: 'old-home', element: <Navigate to="/" /> },
+// export const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <App />,
+//         children: [
+//             { path: '', element: <Home /> },
+//             { path: 'about', element: <About /> },
+//             { path: 'contact', element: <Contact /> },
+//             { path: 'old-home', element: <Navigate to="/" /> },
+//             //   404 route
+//             { path: '*', element: <NotFound /> },
+//         ],
+//     },
+
+// ]);
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route index element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='old-home' element={<Navigate to="/" />} />
             //   404 route
-            { path: '*', element: <NotFound /> },
-        ],
-    },
+            <Route path='*' element={<NotFound />} />
+        </Route>
+    )
 
-]);
-
+);
